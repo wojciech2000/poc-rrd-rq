223a6f8540307a7ba9c01f4c32fbe9dc46c4e3c2
Wojciech Antonik <wojtekantonik9@gmail.com>
2024-05-14 10:56:27 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,57 +1,75 @@
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   createRoutesFromElements,
-  Route, 
-  RouterProvider
-} from 'react-router-dom'
+  Outlet,
+  Route,
+  RouterProvider,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Faq from './pages/help/Faq'
-import Contact, { contactAction } from './pages/help/Contact'
-import NotFound from './pages/NotFound'
-import Careers, { careersLoader } from './pages/careers/Careers'
-import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails"
-import CareersError from './pages/careers/CareersError'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/help/Faq";
+import Contact, { contactAction } from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
+import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
+import CareersError from "./pages/careers/CareersError";
 
 // layouts
-import RootLayout from './layouts/RootLayout'
-import HelpLayout from './layouts/HelpLayout'
-import CareersLayout from './layouts/CareersLayout'
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import { lazy, Suspense } from "react";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="help" element={<HelpLayout />}>
-        <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact/>} action={contactAction} />
-      </Route>
-      <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />}>
-        <Route 
-          index 
-          element={<Careers />} 
-          loader={careersLoader}
-          // errorElement={<CareersError />}
-        />
-        <Route 
-          path=":id" 
-          element={<CareerDetails />}
-          loader={careerDetailsLoader}
-        />
-      </Route>
+const CareersLayout = lazy(() => import("./layouts/CareersLayout"));
 
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-)
+const getRouter = queryClient =>
+  createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<HelpLayout />}>
+          <Route path="faq" element={<Faq />} />
+          <Route path="contact" element={<Contact />} action={contactAction} />
+        </Route>
+        <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />}>
+          <Route
+            index
+            element={<Careers />}
+            loader={() => careersLoader(queryClient)}
+            // errorElement={<CareersError />}
+          />
+          <Route path=":id" element={<CareerDetails />} loader={careerDetailsLoader} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Route>,
+    ),
+  );
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>LAZY COMPONENTS LOADING</div>}>
+        <RouterProvider router={getRouter(queryClient)} />
+      </Suspense>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
-export default App
+export default App;
--- src/pages/careers/Careers.js
+++ src/pages/careers/Careers.js
@@ -1,27 +1,104 @@
-import { Link, useLoaderData } from "react-router-dom"
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import { Await, Link, defer, useLoaderData } from "react-router-dom";
+
+const QUERY_KEY = "contacts";
+
+const commonQueryParams = {
+  queryKey: [QUERY_KEY],
+  queryFn: () => fetch("http://localhost:4000/careers").then(res => res.json()),
+};
 
 export default function Careers() {
-  const careers = useLoaderData()
+  const { data, refetch } = useQuery({
+    ...commonQueryParams,
+    enabled: false,
+  });
+
+  const { isData, packageLocationData } = useLoaderData();
+
+  useEffect(() => {
+    if (isData) {
+      refetch();
+    }
+  }, [isData]);
 
   return (
     <div className="careers">
-      {careers.map(career => (
-        <Link to={career.id.toString()} key={career.id}>
-          <p>{career.title}</p>
-          <p>Based in {career.location}</p>
-        </Link>
-      ))}
+      <React.Suspense fallback={<p>LOADING IN CAREERS</p>}>
+        <Await resolve={packageLocationData} errorElement={<p>Error loading package location!</p>}>
+          {packageLocationData => {
+            const finalData = isData ? data : packageLocationData;
+
+            return finalData.map(career => (
+              <Link to={career.id.toString()} key={career.id}>
+                <p>{career.title}</p>
+                <p>Based in {career.location}</p>
+              </Link>
+            ));
+          }}
+        </Await>
+      </React.Suspense>
     </div>
-  )
+  );
 }
+
+const data = [
+  {
+    id: 1,
+    title: "Senior React Developer",
+    salary: 50000,
+    location: "London, UK",
+  },
+  {
+    id: 2,
+    title: "Plumber",
+    salary: 40000,
+    location: "Bowser's Castle",
+  },
+  {
+    id: 3,
+    title: "Gym Leader",
+    salary: 75000,
+    location: "Kanto Region",
+  },
+  {
+    id: 4,
+    title: "Vue Developer",
+    salary: 40000,
+    location: "Liverpool, UK",
+  },
+  {
+    id: 5,
+    title: "Tutorial Maker",
+    salary: 35000,
+    location: "Manchester, UK",
+  },
+  {
+    id: 6,
+    title: "Website Manager",
+    salary: 50000,
+    location: "Berlin, Germany",
+  },
+  {
+    id: 7,
+    title: "Food Tester",
+    salary: 30000,
+    location: "London, UK",
+  },
+];
 
 // data loader
-export const careersLoader = async () => {
-  const res = await fetch('http://localhost:4000/careers')
+export const careersLoader = async queryClient => {
+  const cachedData = queryClient.getQueryData(QUERY_KEY);
 
-  if (!res.ok) {
-    throw Error('Could not fetch the list of careers')
-  }
+  const getCareers = () =>
+    queryClient.fetchQuery({
+      ...commonQueryParams,
+    });
 
-  return res.json()
-}
+  return defer({
+    isData: !!cachedData,
+    packageLocationData: cachedData ? new Promise(resolve => resolve(data)) : getCareers(),
+  });
+};
